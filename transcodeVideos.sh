1d51b676fd7a54c4ae52512485af34c4e5e497b4
Pat Lambe <pat@maru.org.uk>
2019-06-12 15:49:04 +0100

    should loop forever

--- transcodeVideos.sh
+++ transcodeVideos.sh
@@ -1,64 +1,72 @@
 #!/bin/bash
 
+#make sure another copy isn't running
 if pgrep -x "transcodeVideo" > /dev/null
 then
     exit 0
 fi
 
+#find $REMOTE_DIR -type f \( -iname '*.ts' -or  -iname '*.mkv'  -or  -iname '*.mp4' -not -iname '*-trailer.*' \)
+
+
 LOCAL_DIR="/data"
 REMOTE_DIR="/movies"
 
-#if the transcode list is empty or missing build a new list
-if [ ! -s /data/to_be_transcoded.txt ]; then
-    touch /data/to_be_transcoded.txt
-    find $REMOTE_DIR -name '*.mkv' |  grep -v 'trailer\|NEW' | while read line; do     
-        filename=$(basename "$line")
+while [1]; do
+
+    #if the transcode list is empty or missing build a new list
+    if [ ! -s /data/to_be_transcoded.txt ]; then
+        touch /data/to_be_transcoded.txt
+        find $REMOTE_DIR -type f \( -iname '*.ts' -or  -iname '*.mkv'  -or  -iname '*.mp4' -not -iname '*-trailer.*' -not -iname '*-NEW.*' \) | while read line; do     
+            filename=$(basename "$line")
+            extension="${filename##*.}"
+            filename="${filename%.*}"
+            remoteDir=$(dirname "$line")
+            
+            if [ -f "$remoteDir/compressed" ]; then
+                echo "ignoring $line, found compressed"
+                continue
+            elif [ -f "$remoteDir/$filename.compressed" ]; then
+                echo "ignoring $line, found $filename.compressed"
+                continue
+            fi
+
+            echo $line >> /data/to_be_transcoded.txt
+        done
+    fi
+
+    #process until we've emptied the file
+    while [ -s /data/to_be_transcoded.txt ]; do
+        
+        #transcode the top file in the transcode list file
+        file=$(head -n 1 /data/to_be_transcoded.txt)
+        filename=$(basename "$file")
         extension="${filename##*.}"
         filename="${filename%.*}"
-        remoteDir=$(dirname "$line")
-        
-        if [ -f "$remoteDir/compressed" ]; then
-            echo "ignoring $line, found compressed"
-            continue
-        elif [ -f "$remoteDir/$filename.compressed" ]; then
-            echo "ignoring $line, found $filename.compressed"
-            continue
-        elif [ -f "$remoteDir/$filename.compressed" ]; then
-            echo "ignoring $line, found $filename.compressed"
-            continue
-        fi
+        remoteDir=$(dirname "$file")
 
-        echo $line >> /data/to_be_transcoded.txt
-    done
-fi
+        echo "Processing file '$file'"; 
 
-#transcode the top file in the transcode list file
-file=$(head -n 1 /data/to_be_transcoded.txt)
-filename=$(basename "$file")
-extension="${filename##*.}"
-filename="${filename%.*}"
-remoteDir=$(dirname "$file")
+        localSrcFile=$LOCAL_DIR/$filename.$extension
+        localTargetFile=$LOCAL_DIR/$filename-NEW.$extension
+        remoteTargetFile=$remoteDir/$filename-NEW.$extension
+        remoteTagFile=$remoteDir/$filename.compressed
 
-echo "Processing file '$file'"; 
+        #copy file locally
+        cp $file $localSrcFile
 
-localSrcFile=$LOCAL_DIR/$filename.$extension
-localTargetFile=$LOCAL_DIR/$filename-NEW.$extension
-remoteTargetFile=$remoteDir/$filename-NEW.$extension
-remoteTagFile=$remoteDir/$filename.compressed
+        #transcode
+        echo "" | transcode-video --preset faster --quick $localSrcFile -o $localTargetFile #note, the echo is to stop tv hijacking stdin and stopping the loop
 
-#copy file locally
-cp $file $localSrcFile
+        #copy file back to nfs
+        cp $localTargetFile $remoteTargetFile
 
-#transcode
-echo "" | transcode-video --preset faster --quick $localSrcFile -o $localTargetFile #note, the echo is to stop tv hijacking stdin and stopping the loop
+        #mark as processed
+        touch $remoteTagFile
 
-#copy file back to nfs
-cp $localTargetFile $remoteTargetFile
-
-#mark as processed
-touch $remoteTagFile
-
-#remove the movie from the file
-tail -n +2 "/data/to_be_transcoded.txt" > "/data/to_be_transcoded.tmp" && mv  "/data/to_be_transcoded.tmp" "/data/to_be_transcoded.txt"
-
-
+        #remove the movie from the file
+        tail -n +2 "/data/to_be_transcoded.txt" > "/data/to_be_transcoded.tmp" && mv  "/data/to_be_transcoded.tmp" "/data/to_be_transcoded.txt"
+    done;
+    
+    sleep 1d
+done;
